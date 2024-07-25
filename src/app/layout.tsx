354f41c31535edd72b40728d4f1879b4e7b87a05
tinysheep007 <pzxiaomie@gmail.com>
2024-07-24 21:31:57 -0400

    added google analytic script in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,15 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Head from "next/head";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Zihe(Peter) Zhang",
-  description: "Zihe(Peter) Zhang's Portfolio Page",
+  title: "Zihe (Peter) Zhang",
+  description: "Zihe (Peter) Zhang's Portfolio Page",
 };
+
+const GTAG_ID = "G-9SDEMV4C4H"; // Replace with your actual Google Tag ID
 
 export default function RootLayout({
   children,
@@ -19,6 +20,16 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" type="image/png" sizes="32x32" href="/images/glasses.png" />
+
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${GTAG_ID}`}></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GTAG_ID}');
+          `
+        }}></script>
       </head>
       <body className={inter.className}>{children}</body>
     </html>
